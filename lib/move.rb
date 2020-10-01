7e54e50bc16a69ce5565b88b57edad136ce8f0c2
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 05:08:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -14,10 +14,17 @@ def input_to_index(input)
   index = input.to_i - 1
 end
 
-
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 board = [" ", " ", " "]
 def move(board, index, value)
     board[index] = value
+    display_board
 end
 
     update_array_at_with(board, 0, "X")
