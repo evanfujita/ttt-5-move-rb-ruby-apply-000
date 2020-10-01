25f6c349829f99dad70733a006a00c6a40c80a90
magical-tutorial-4913 <evanfujita@gmail.com>
2020-10-01 06:06:43 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,8 +8,7 @@ def display_board(board)
 end
 
 def input_to_index(input)
-  input_to_index = input.to_i - 1
-  index = input_to_index
+  index = input.to_i - 1
 end
 
 def move(board, index, value = "X")
